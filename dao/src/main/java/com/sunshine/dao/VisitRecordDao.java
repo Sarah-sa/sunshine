6b7128fb7a1beeacae468f4dd71c6adeba8cbb5b
云和数据-王一贺 <546092228@qq.com>
2017-03-09 19:27:36 +0800

    回访页面

--- dao/src/main/java/com/sunshine/dao/VisitRecordDao.java
+++ dao/src/main/java/com/sunshine/dao/VisitRecordDao.java
@@ -4,51 +4,41 @@ import java.util.List;
 import com.sunshine.model.VisitRecord;
 
 /**
- * 走访记录模型的数据访问接口
- * 
+ * 走访记录模型的数据访问接口 
  * @author 王一贺 2017年3月7日
  */
 public interface VisitRecordDao {
 
 	/**
 	 * 根据id获得对应的走访记录
-	 * 
-	 * @param id
-	 *            走访记录的id
+	 * @param id  走访记录的id
 	 * @return 走访记录
 	 */
 	VisitRecord getVisitRecord(String id);
 
 	/**
 	 * 获得所有的走访记录
-	 * 
 	 * @return 走访记录的list集合
 	 */
 	List<VisitRecord> listVisitRecord();
 
 	/**
 	 * 插入 走访记录 到数据库
-	 * 
-	 * @param vr
-	 *            走访记录对象
+	 * @param vr 走访记录对象
 	 * @return 数据库中受影响的行数
 	 */
 	int saveVisitRecord(VisitRecord vr);
 
 	/**
 	 * 更新 走访记录
-	 * 
-	 * @param vr
-	 *            走访记录对象
+	 * @param vr 走访记录对象
 	 * @return 数据库中受影响的行数
 	 */
 	int updateVisitRecord(VisitRecord vr);
 
 	/**
 	 * 删除 走访记录
-	 * 
-	 * @param id
-	 *            要删除的走访记录的id
+	 * @param id  要删除的走访记录的id
 	 * @return 数据库中受影响的行数
 	 */
 	int removeVisitRecord(String id);
--- web/src/main/webapp/WEB-INF/views/ftl/head.ftl
+++ web/src/main/webapp/WEB-INF/views/ftl/head.ftl
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="http://g.alicdn.com/sj/dpl/1.5.1/js/sui.min.js"></script>
+<script type="text/javascript" src="/js/sui.min.js"></script>
 <script type="text/javascript" src="/uicomponent/layer/layer.js"></script>
 <script type="text/javascript" src="/js/vue.js"></script>
-<link rel="stylesheet" type="text/css" href="http://g.alicdn.com/sj/dpl/1.5.1/css/sui.min.css">
+<link rel="stylesheet" type="text/css" href="/css/sui.min.css">
