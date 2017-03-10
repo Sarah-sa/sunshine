d89082e2c8a9b2016b8dafb778f6fbc77d9e24f9
云和数据-孙爱平 <liesycat>
2017-03-10 18:36:55 +0800

    提交尿酸模块

---
+++ service/src/main/java/com/sunshine/service/impl/BloodLipidsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.sunshine.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sunshine.dao.BloodLipidsDao;
+import com.sunshine.model.BloodLipids;
+import com.sunshine.service.BloodLipidsService;
+	/**
+	 * @author 云和数据-孙爱平
+	 *
+	 */
+	@Service
+public class BloodLipidsServiceImpl implements BloodLipidsService {
+	@Autowired
+	private BloodLipidsDao bloodLipidsDao;
+	@Override
+	public List<BloodLipids> getBloodLipids(Date sdate, Date edate) {
+		// TODO Auto-generated method stub
+		return bloodLipidsDao.getBloodLipids(sdate, edate);
+	}
+
+}
