669bd9af48b8bc2ced5e7543bfded49dd1ed7303
云和数据-齐铁成 <lieslycat>
2017-03-14 15:31:50 +0800

    云和数据—齐铁成 tips知识库分类和知识库

---
+++ service/src/main/java/com/sunshine/service/impl/TipsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.sunshine.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sunshine.dao.TipsDao;
+import com.sunshine.model.Tips;
+import com.sunshine.service.TipsService;
+
+@Service
+public class TipsServiceImpl implements TipsService{
+
+	@Autowired
+	private TipsDao td;
+	
+	@Override
+	public List<Tips> getTipsList(String tips_ctgy_id) {
+		// TODO Auto-generated method stub
+		return td.getTipsList(tips_ctgy_id) ;
+	}
+
+	@Override
+	public List<Tips> getLikeTips(String Titlename) {
+		// TODO Auto-generated method stub
+		return td.getLikeTips(Titlename);
+	}
+	
+
+}
