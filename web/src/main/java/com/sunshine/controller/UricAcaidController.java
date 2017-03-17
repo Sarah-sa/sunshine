a69719d03e3f352aed9261d5ae9d6fe0c9394ce8
云和数据-孙爱平 <lieslycat>
2017-03-17 18:06:02 +0800

    初次提交前端页面及修改控制层

--- web/src/main/java/com/sunshine/controller/UricAcaidController.java
+++ web/src/main/java/com/sunshine/controller/UricAcaidController.java
@@ -1,23 +1,56 @@
 package com.sunshine.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sun.tools.example.debug.expr.ParseException;
+import com.sunshine.model.BloodSugar;
 import com.sunshine.model.UricAcaid;
 import com.sunshine.service.UricAcaidService;
 
 /**
-	 * 
-	 * @author 云和数据-孙爱平
-	 *
-	 */
+ * 
+ * @author 云和数据-孙爱平
+ *
+ */
+	@RestController
 public class UricAcaidController {
-	
-	private UricAcaidService uricAcaidService;
-	@RequestMapping("/getUricAcaid")
-	String findUricAcaid(Date sdate,Date edate){
-		List<UricAcaid> uricAcaid=(List<UricAcaid>) uricAcaidService.getUricAcaid(sdate, edate);
-		return "a";
 		
+	@Autowired
+	private UricAcaidService uricAcaidService;
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+	}
+
+	
+	@RequestMapping(value="/getUricAcaid",method=RequestMethod.GET)
+	public ModelAndView listUricAcaid(Date sdate, Date edate, HttpServletRequest req) throws ParseException{
+			
+		List<UricAcaid> uricAcaid=uricAcaidService.getUricAcaid(sdate, edate);
+		Map<String,Object> data = new HashMap<String,Object>(); 
+		data.put("UricAcaid",uricAcaid);
+	     
+			
+			return new ModelAndView("uricacaid",data); 
+			
+		
+	
 	}
 }
