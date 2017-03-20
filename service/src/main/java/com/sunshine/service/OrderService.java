7840ce9b110060964a029f7caead9a4d521c23cd
云和数据-王辉 <lieslycat>
2017-03-20 09:43:43 +0800

    添加获取服务分类的接口，暂时将shiro注释掉

--- service/src/main/java/com/sunshine/service/OrderService.java
+++ service/src/main/java/com/sunshine/service/OrderService.java
@@ -3,6 +3,8 @@ package com.sunshine.service;
 import java.util.List;
 import java.util.Map;
 
+import com.sunshine.model.ServCategory;
+
 /**
  * 订单操作类
  * @author 云和数据-王辉
@@ -18,4 +20,23 @@ public interface OrderService {
 	 */
 	List<Map<String, Object>> getOrderSummary(int pageIndex, int pageSize);
 	
+	/**
+	 * 获取订单详情
+	 * @param id 订单id
+	 * @return
+	 */
+	Map<String, Object> getOrderDetail(String id);
+	
+	/**
+	 * 获取一级服务分类
+	 * @return 一级服务分类
+	 */
+	List<ServCategory> listRootCategory();
+	
+	/**
+	 * 获取所有子分类
+	 * @param pid 子分类
+	 * @return
+	 */
+	List<ServCategory> listChildCategory(String pid);
 }
